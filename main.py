d8ca4a494c21ea0e963b0774294862e411b0e431
horymzminecraft@gmail.com <horymzminecraft@gmail.com>
2026-02-28 10:54:55 +0100

    help

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ from robolab_turtlebot import Turtlebot, Rate, get_time
 bumper_names = ['LEFT', 'CENTER', 'RIGHT']
 state_names = ['RELEASED', 'PRESSED']
 
-
-bumper_names = ['LEFT', 'CENTER', 'RIGHT']
-state_names = ['RELEASED', 'PRESSED']
-
+StateofBumper = 0
 
 def bumper_cb(msg):
     """Bumber callback."""
+
+    global StateofBumper
     # msg.bumper stores the id of bumper 0:LEFT, 1:CENTER, 2:RIGHT
     bumper = bumper_names[msg.bumper]
 
     # msg.state stores the event 0:RELEASED, 1:PRESSED
     state = state_names[msg.state]
+    if msg.state == 1:
+        StateofBumper = 1
+    else: 
+        StateofBumper = 0
 
     # Print the event
     print('{} bumper {}'.format(bumper, state))
@@ -25,15 +28,18 @@ def bumper_cb(msg):
 
 def main():
     # Initialize turtlebot class
-    turtle = Turtlebot(rgb=True, depth=True, )
+    turtle = Turtlebot(rgb=True, depth=True, pc=True)
 
-    press = turtle.register_bumper_event_cb(bumper_cb)
+    rate = Rate(10)
+    turtle.register_bumper_event_cb(bumper_cb)
     t = get_time()
     
-    rate = Rate(10)
-    while ((get_time() - t) < 20) and turtle.register_bumper_event_cb(bumper_cb != "PRESSED"):
-         
-        turtle.cmd_velocity(linear=0.1)
+    while ((get_time() - t) < 20) and (StateofBumper == 0):
+            
+        turtle.get_rgb_image()
+        turtle.get_depth_image()
+        turtle.register_bumper_event_cb(bumper_cb)
+        turtle.cmd_velocity(linear=0.05)
         rate.sleep()
 
 
