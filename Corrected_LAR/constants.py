64361f385f8f89f0b72929dace93c9fa386b8df8
danijel67 <danielberan1@gmail.com>
2026-03-22 20:44:53 +0100

    Update constants.py

--- Corrected_LAR/constants.py
+++ Corrected_LAR/constants.py
@@ -5,11 +5,13 @@ import numpy as np
 linear_0 = 0
 linear_around_the_ball = 0.14
 linear_the_rest = 0.08
+
 # Angular velocity ------------------------------------
 angular_0 = 0
 angular_spinning = -np.pi/20
 angular_around_the_ball = 0.4
 angular_quater_spin = -np.pi/8
+
 # Other -----------------------------------------------
 stop_distance = 0.47
 IMG_CENTER_X = 334
