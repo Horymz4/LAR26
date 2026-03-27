72890fdaf9dd51d6e8f92df231f8fefb88a06e76
horymzminecraft@gmail.com <horymzminecraft@gmail.com>
2026-03-27 17:07:37 +0100

    new

--- newcorr/utils.py
+++ newcorr/utils.py
@@ -40,7 +40,7 @@ def calibrate(turtle):
 
 # Image reasoning and image utils ---------------------
 def reasoning(pos,radius,avg_x,dist, ratio):
-    if (not exited_garage.is_set()) and ratio is not None and  ratio < 0.5:
+    if (not exited_garage.is_set()) and ratio is not None and  ratio < 0.55:
         exited_garage.set()
     if (not garage_stage.is_set()) and radius is not None and pos is not None and 150 > radius > 5  and IMG_CENTER_X + 30 > pos[0] > IMG_CENTER_X - 30:
         garage_stage.set()
@@ -71,7 +71,7 @@ def garage_image(rgb,ref,turtle):
     return avg_x, dist
 
 # to see if we are in garage
-def garage_wall_percentage(pc, dist = 0.8):
+def garage_wall_percentage(pc, dist = 0.7):
     if pc is None:
         return None
 
