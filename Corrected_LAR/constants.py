7d72082def72b613306be5f1e3ade516387103f8
horymzminecraft@gmail.com <horymzminecraft@gmail.com>
2026-03-27 12:10:02 +0100

    a

--- Corrected_LAR/constants.py
+++ Corrected_LAR/constants.py
@@ -3,13 +3,13 @@ import numpy as np
 
 # Linear velocity -------------------------------------
 linear_0 = 0
-linear_around_the_ball = 0.16
+linear_around_the_ball = 0.12
 linear_the_rest = 0.08
 
 # Angular velocity ------------------------------------
 angular_0 = 0
 angular_spinning = -np.pi/20
-angular_around_the_ball = 0.4
+angular_around_the_ball = 0.32
 angular_quater_spin = -np.pi/8
 
 # Other -----------------------------------------------
--- Corrected_LAR/utils.py
+++ Corrected_LAR/utils.py
@@ -49,7 +49,7 @@ def reasoning(pos,radius,avg_x,dist):
     if (not see_garage.is_set()) and avg_x is not None and IMG_CENTER_X + 30 > avg_x > IMG_CENTER_X - 30:
         see_garage.set()
         print("SEE GARAGE")
-    if (not ending_stage.is_set()) and dist is not None and see_garage.is_set() and dist < 2:
+    if (not ending_stage.is_set()) and dist is not None and see_garage.is_set() and dist < 1.1:
         ending_stage.set()
         print("PARKING")
 
