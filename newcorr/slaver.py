5d68af7b57da1ae69fd1fc1416a6cca61efec006
horymzminecraft@gmail.com <horymzminecraft@gmail.com>
2026-04-10 16:28:21 +0200

    heelp

--- newcorr/slaver.py
+++ newcorr/slaver.py
@@ -64,7 +64,7 @@ def obraz(turtle,ref_green, ref_purple):
                 processing_image.set()
             # Stage 5
             elif not ending_stage.is_set():
-                # turtle.wait_for_rgb_image()
+                turtle.wait_for_rgb_image()
                 rgb = turtle.get_rgb_image()
                 avg_x, dist = garage_image(rgb, ref_purple, turtle)
                 pos = radius = None
