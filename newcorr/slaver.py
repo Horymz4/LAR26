279e5124cea428696f14870a9211bbfa1f66d6b2
horymzminecraft@gmail.com <horymzminecraft@gmail.com>
2026-04-10 15:55:22 +0200

    heelp

--- newcorr/slaver.py
+++ newcorr/slaver.py
@@ -21,10 +21,10 @@ def pohyb(turtle):
     print("Start pohyboveho vlakna")
     rate = Rate(10)
  
-    if not StateofBumper.is_set(): stage1(turtle, rate)  # find opening, exit
-    if not StateofBumper.is_set(): stage2(turtle, rate)  # spin to find ball
-    if not StateofBumper.is_set(): stage3(turtle, rate)  # drive to ball
-    if not StateofBumper.is_set(): stage4(turtle, rate)  # circumnavigate
+    # if not StateofBumper.is_set(): stage1(turtle, rate)  # find opening, exit
+    # if not StateofBumper.is_set(): stage2(turtle, rate)  # spin to find ball
+    # if not StateofBumper.is_set(): stage3(turtle, rate)  # drive to ball
+    # if not StateofBumper.is_set(): stage4(turtle, rate)  # circumnavigate
     if not StateofBumper.is_set(): stage5(turtle, rate)  # find + approach garage
     if not StateofBumper.is_set(): stage6(turtle, rate)  # park
  
