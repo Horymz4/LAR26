019b4db2944a16b20a4e4cdc500f965511502dda
horymzminecraft@gmail.com <horymzminecraft@gmail.com>
2026-04-10 20:10:23 +0200

    heelp

--- newcorr/stages.py
+++ newcorr/stages.py
@@ -49,7 +49,7 @@ def stage2(turtle,rate):
 
     move_until(
         turtle, rate,
-        linear_0,-angular_spinning-0.3,
+        linear_0,-angular_spinning,
         lambda: not garage_stage.is_set(),
         "Stage 2",
         # ang_speed_reg=lambda:P_reg_ball_spinning(-angular_spinning-0.3)
