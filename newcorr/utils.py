a903b5953d0dec9d56a1f868902c8f670ec5cd79
horymzminecraft@gmail.com <horymzminecraft@gmail.com>
2026-04-11 11:10:04 +0200

    heelp

--- newcorr/utils.py
+++ newcorr/utils.py
@@ -41,9 +41,10 @@ def calibrate(turtle):
 
 # Image reasoning and image utils ---------------------
 def reasoning(pos,radius,avg_x,dist, ratio):
+    THRESHOLD = 600/radius
     if (not exited_garage.is_set()) and ratio is not None and  ratio < 0.15:
         exited_garage.set()
-    if (not garage_stage.is_set()) and radius is not None and pos is not None and 150 > radius > 5  and IMG_CENTER_X + 30 > pos[0] > IMG_CENTER_X - 30:
+    if (not garage_stage.is_set()) and radius is not None and pos is not None and 150 > radius > 5  and IMG_CENTER_X + THRESHOLD > pos[0] > IMG_CENTER_X - THRESHOLD:
         garage_stage.set()
         print("SEE BALL")
 
