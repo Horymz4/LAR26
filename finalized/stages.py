5cdb2001f9c8eafb2011a1d9d3b40cf354f25d69
horymzminecraft@gmail.com <horymzminecraft@gmail.com>
2026-04-15 17:13:27 +0200

    idk man

--- finalized/stages.py
+++ finalized/stages.py
@@ -226,7 +226,7 @@ def turn_to_garage(turtle, rate):
     move_until(
         turtle, rate,
         linear_0, angular_spinning * m * direction,
-        condition_fn=lambda: cond_angle(turtle, np.pi, 0.05, t),
+        condition_fn=lambda: cond_angle(turtle, np.pi-0.05, 0.1, t),
         text="sping towards garage"
     )
 
